a8fb9df798fdb8891822372cf2bb6243683f4271
Todd Walraven and Zakkrey Short <todd@>
2020-02-13 14:59:17 -0800

    refactor weather api call for error handling

--- src/mars-weather.js
+++ src/mars-weather.js
@@ -2,10 +2,15 @@ export class Mars {
     async getWeather() {
         try{
             let answer = await fetch (`https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0`);
-            let jsonifiedAnswer = await answer.json();
+            let jsonifiedAnswer;
+            if (Response.ok && Response.status == 200) {
+                jsonifiedResponse = await answer.json();
+            } else {
+                jsonifiedAnswer = false;
+            }
             return jsonifiedAnswer;
         } catch(error){
-            console.error("there was an error handing your request:" + error.message)
+            return false;
         }
     };
 }
